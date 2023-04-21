d01462438560202ae020ece2e1de1fbe30292779
dionkb <dionkbaskara@gmail.com>
2023-04-21 13:13:35 -0400

    prompt now runs questions when initialized via 'node index.js'

--- Develop/index.js
+++ Develop/index.js
@@ -5,22 +5,86 @@ const fs = require('fs');
 
 // // TODO: Create an array of questions for user input
 const questions = [
-    "Project Title:",
-    "Short summary:",
-    "In-depth description of features:",
-    "Include space for repository url?", // Y or N
-    "List a dependency?", // Y or N
-    "Dependecy:",
-    "List another dependency?", // Y or N
-    "Installation instructions:",
-    "Include space for deployed application url?", // Y or N
-    "Provide an author:",
-    "Provide additional authors?", // Y or N
-    "Use generic 'See the LICENSE.md file for details'?", // Y or N
-    "Provide license details:",
-    "Do you need to list any acknowledgements?", // Y or N
-    "Acknowledgement:",
-    "Any more acknowledgements?", // Y or N
+    {
+    name: "title",
+    message: "Enter project title:",
+    type: "input",
+    },
+    {
+    name: "summary",
+    message: "Enter a short summary:",
+    type: "input",
+    },
+    {
+    name: "full description",
+    message: "In-depth description of features:",
+    type: "input",
+    },
+    {
+    name: "include repository",
+    message: "Include space for repository url?", // Y or N
+    type: "confirm",
+    },
+    {
+    name: "dependencies start",
+    message: "List a dependency?",
+    type: "confirm",
+    },
+    {
+    name: "list dependencies",
+    message: "List a dependency:",
+    type: "input",
+    },
+    {
+    name: "more dependencies",
+    message: "List another dependency?",
+    type: "confirm",
+    },
+    {
+    name: "installation",
+    message: "Installation instructions:",
+    type: "input",
+    },
+    {
+    name: "include depoloyed app url",
+    message: "Include space for deployed application url?",
+    type: "confirm",
+    },
+    {
+    name: "author",
+    message: "Provide an author:",
+    type: "string",
+    },
+    {
+        name: "additional authors",
+    message: "Provide additional authors?",
+    type: "confirm",
+    },
+    {
+        name: "Use generic license",
+    message: "Use generic 'See the LICENSE.md file for details'?",
+    type: "confirm",
+    },
+    {
+        name: "unique license details",
+    message: "Provide license details:",
+    type: "string",
+    },
+    {
+        name: "acknowledgements",
+    message: "Do you need to list any acknowledgements?",
+    type: "confirm",
+    },
+    {
+        name: "first acknowledgement",
+    message: "Acknowledgement:",
+    type: "string",
+    },
+    {
+        name: "author",
+    message: "Any more acknowledgements?",
+    type: "confirm",
+    },
 ];
 
 // // TODO: Create a function to write README file
@@ -29,15 +93,14 @@ function writeToFile(fileName, data) {
         err ? console.error(err) : console.log('Success!')
     );
 }
-
 // testing code above
 //writeToFile();
 
-
-
-
 // // TODO: Create a function to initialize app
-// function init() {}
+function init() {}
+
+inquirer
+    .prompt(questions)
 
 // // Function call to initialize app
 // init();
