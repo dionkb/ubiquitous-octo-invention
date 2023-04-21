1ff9bc3a9585b324ac9c23fc0b454aac8f137971
dionkb <dionkbaskara@gmail.com>
2023-04-21 12:11:25 -0400

    wrote function to create a blank README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // // TODO: Create an array of questions for user input
 const questions = [
@@ -23,7 +24,17 @@ const questions = [
 ];
 
 // // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', "test", (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
+
+// testing code above
+//writeToFile();
+
+
+
 
 // // TODO: Create a function to initialize app
 // function init() {}
