f808df8ff314322c9d0b4d587212f6d267fb1c08
dionkb <dionkbaskara@gmail.com>
2023-04-21 22:14:27 -0400

    Badges are generated for test license chosen. Fill in remainder

--- Develop/index.js
+++ Develop/index.js
@@ -181,6 +181,36 @@ const questions = [
         }
     },
     {
+        name: "license",
+        message: "Choose a license to use:",
+        type: "list",
+        default: "none",
+        choices: [
+            "none",
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "BSD 2-Clause 'Simplified' License",
+            "BSD 3-Clause 'New' or 'Revised' License",
+            "Boost Software License 1.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU Affero General Public License v3.0",
+            "GNU General Public License v2.0",
+            "GNU Lesser General Public License v2.1",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+        ],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('You must enter license information');
+                return false;
+            }
+        }
+    },
+    {
         name: "licenseDetails",
         message: "Provide license details:",
         type: "input",
